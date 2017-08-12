58cf2c2841113c7695eb9144200726bffedbb59d
Matt Wagner <matt.a.wagner4@gmail.com>
2017-08-11 20:44:51 -0400

    Initial Commit
    This initial commit has webpacker up and running in it's current state
    for stylesheets and javascript. It also removes the assets pipeline.

---
+++ app/assets/octiv-wp/images/index.js
@@ -0,0 +1,6 @@
+// ===========================================================
+// Make images known by webpack by including them here.
+// ===========================================================
+// import './logo.svg';
+// import './menu-open.svg';
+// import './menu-close.svg';
---
+++ app/assets/octiv-wp/index.js
@@ -0,0 +1,3 @@
+import './stylesheets/application.scss';
+
+alert('Sup Brohemoth');
